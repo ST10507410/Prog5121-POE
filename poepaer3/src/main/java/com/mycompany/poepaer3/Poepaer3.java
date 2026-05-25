2c72f9e953a581413a89062855f4a6fcd445707e
ST10507410 <sethemaa@icloud.com>
2026-05-25 23:00:23 +0200

    Add files via upload

---
+++ poepaer3/src/main/java/com/mycompany/poepaer3/Poepaer3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poepaer3;
+
+/**
+ *
+ * @author sethe
+ */
+public class Poepaer3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
